c2e64f3e2d14dea204cafa80a850f176b6740bac
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2023-05-27 14:28:42 -0400

    Initial commit

---
+++ word-counter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  word-counter
+//
+//  Created by Ali Sayed Salehi on 2023-05-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
