4b7f1b0b00b0f5877ad64dbfd0171db7c61d4081
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2023-05-29 03:09:33 -0400

    create NumList class

--- word-counter/main.cpp
+++ word-counter/main.cpp
@@ -1,8 +1,77 @@
 
-#include <iostream>
+//#include <iostream>
+//#include "NumList.hpp"
+//
+//using std::cout;
+//using std::endl;
+//
+//int main(int argc, const char * argv[]) {
+//
+//    NumList numList;
+//
+//
+//    cout << "all good\n";
+//    cout << numList.getCapacity() << endl;
+//    numList.append(2);
+//    numList.append(2);
+//
+//    cout << numList.contains(3) << endl;
+//    cout << numList.getCapacity() << endl;
+//    numList.append(5);
+//    numList.append(4);
+//    numList.append(4);
+//    numList.print(cout);
+//
+//
+//    return 0;
+//}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+#include <iostream>
+#include <cassert>
+#include "inc/NumList.hpp"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    std::cout << "Testing NumList!\n";
+    NumList list{};
+    cout << "list-1 -> " << list << endl;
+    assert(list.getCapacity() == 1);
+
+    list.append(19);
+    cout << "list-2 -> " << list << endl;
+    assert(list.getCapacity() == 1);
+    assert(list.getSize() == 1);
+
+    list.append(32);
+    cout << "list-3 -> " << list << endl;
+    assert(list.getCapacity() == 2);
+    assert(list.getSize() == 2);
+
+    list.append(21);
+    cout << "list-4 -> " << list << endl;
+    assert(list.getCapacity() == 4);
+    assert(list.getSize() == 3);
+
+    list.append(7);
+    cout << "list-5 -> " << list << endl;
+    assert(list.getCapacity() == 4);
+    assert(list.getSize() == 4);
+
+    list.append(18);
+    cout << "list-6 -> " << list << endl;
+    assert(list.getCapacity() == 8);
+    assert(list.getSize() == 5);
+
+    int value = -1;
+    for (int k = 0; k < list.getSize(); ++k) {
+        assert(list.get(k, value));
+        assert(value == list.get(k));
+    }
+
+    assert(! list.get(list.getSize(), value));
+    cout << "NumList Test Successful" << endl;
     return 0;
 }
--- word-counter/src/NumList.cpp
+++ word-counter/src/NumList.cpp
@@ -1,2 +1,121 @@
 
 #include "../inc/NumList.hpp"
+
+NumList::NumList() : capacity(1), pArray(new int[1]), size(0) {
+}
+
+NumList::~NumList() {
+    delete[] pArray;
+}
+
+NumList::NumList(const NumList& source) :
+    capacity(source.capacity), pArray(new int[source.capacity]), size(source.size) {
+
+    for (int i = 0; i < source.size; ++i) {
+        pArray[i] = source.pArray[i];
+    }
+
+}
+
+NumList::NumList(NumList &&source)  noexcept :
+        capacity(source.capacity), pArray(source.pArray), size(source.size) {
+    source.pArray = nullptr;
+}
+
+bool NumList::empty() const {
+    return size == 0;
+}
+
+bool NumList::full() const {
+    return size == capacity;
+}
+
+int NumList::getSize() const {
+    return size;
+}
+
+void NumList::expand() {
+    int* expandedPArray = new int[capacity * 2];
+    for (int i = 0; i < size; ++i) {
+        expandedPArray[i] = pArray[i];
+    }
+    delete[] pArray;
+    pArray = expandedPArray;
+    capacity *= 2;
+}
+
+int NumList::getCapacity() const {
+    return capacity;
+}
+
+bool NumList::contains(int x) const {
+    for (int i =0; i < size; ++i){
+        if (pArray[i] == x) return true;
+    }
+    return false;
+}
+
+void NumList::append(int x) {
+    if (full()) expand();
+    pArray[size] = x;
+    ++size;
+}
+
+NumList& NumList::operator=(const NumList& rhs) {
+    if (this == &rhs) return *this;
+    delete[] pArray;
+    pArray = new int[rhs.capacity];
+
+    for (int i = 0; i < size; ++i) {
+        pArray[i] = rhs.pArray[i];
+    }
+
+    capacity = rhs.capacity;
+    size = rhs.size;
+
+    return *this;
+}
+
+NumList &NumList::operator=(NumList &&rhs) noexcept {
+    if (this == &rhs) return *this;
+
+    delete[] pArray;
+    pArray = rhs.pArray;
+
+    rhs.pArray = nullptr;
+
+    capacity = rhs.capacity;
+    size = rhs.size;
+
+    return *this;
+}
+
+void NumList::print(std::ostream &os) const {
+    for (int i = 0; i < size; ++i) {
+        os << " " << pArray[i] << ",";
+    }
+}
+
+int NumList::get(int index) const {
+    if (index < 0 || index >= size) {
+        throw std::out_of_range("Invalid index");
+    }
+    return pArray[index];
+}
+
+bool NumList::get(int index, int &value) const {
+    if (index < 0 || index >= size) {
+        return false;
+    }
+
+    value = pArray[index];
+    return true;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const NumList& numList) {
+    for (int i = 0; i < numList.size; ++i) {
+        os << " " << numList.pArray[i] << ",";
+    }
+    return os;
+}
